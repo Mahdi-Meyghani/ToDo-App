9cd2f192dfd0f0c759823709f921e1baec9c2d89
Mahdi <mahdimeyghani02@gmail.com>
2024-05-02 19:59:04 +0330

    Implement the Edit button and add the List Box.

--- gui.py
+++ gui.py
@@ -5,19 +5,43 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a To-Do:")
 input_text = sg.InputText(tooltip="Enter a To-Do", key="-IN-")
 add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=todo_functions.get_todos(),
+                      size=(45, 10),
+                      enable_events=True,
+                      key="-LIST-")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_text, add_button]],
-                   font=("Arial", 12))
+                   layout=[[label], [input_text, add_button], [list_box, edit_button]],
+                   font=("Arial", 15))
 
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
+    print(values["-LIST-"])
     match event:
         case "Add":
             todos = todo_functions.get_todos()
-            new_item = values["-IN-"] + "\n"
-            todos.append(new_item)
+            new_todo = values["-IN-"] + "\n"
+            todos.append(new_todo)
             todo_functions.write_todos(todos)
+
+            window["-LIST-"].update(todos)
+        case "Edit":
+            todos = todo_functions.get_todos()
+
+            todo_to_edited = values["-LIST-"][0]
+            index = todos.index(todo_to_edited)
+            new_todo = values["-IN-"]
+            todos[index] = new_todo
+
+            todo_functions.write_todos(todos)
+
+            window["-LIST-"].update(todos)
+        case "-LIST-":
+            item = values["-LIST-"][0]
+            window["-IN-"].update(item)
         case sg.WIN_CLOSED:
             break
 
